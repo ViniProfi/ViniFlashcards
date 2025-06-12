dc0581ce5ed1cb1773fdcf0470b100dae950eb6d
ViniProfi <vinicius.oliveira.santos17@escola.pr.gov.br>
2025-06-12 18:41:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Curiosidade',
+    'Quantos litros de sangue uma pessoa tem?',
+    'Cerca de 5 litros.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual país tem mais fusos horários?',
+    'França com 12 fuso horários.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o maior órgão do corpo humano?',
+    'A pele.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'Quem criou o sistema operacional Linux?',
+    'Linus Torvalds.'
+)
+
+criaCartao(
+    'História do Brasil',
+    'O que foi a Inconfidência Mineira?',
+    'Um movimento contra os impostos da Coroa Portuguesa.'
+)
